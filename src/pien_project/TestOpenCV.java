20faad6db302c059babc63c80f7b2b54779ffacd
mayu <mayu.sidetail0919@gmail.com>
2021-01-14 14:34:09 +0900

    不要なファイルを削除

--- src/pien_project/TestOpenCV.java
+++ src/pien_project/TestOpenCV.java
@@ -13,36 +13,17 @@ public class TestOpenCV {
         ImageIO iio = new ImageIO();
         BasicImageProcessing bip = new BasicImageProcessing();
 
-        //----------------------------------------
-        //DEMO1: resizing and filtering the image
-        //----------------------------------------
-        Mat read_img = iio.readImage("src/face.jpg");
+
+        Mat read_img = iio.readImage("src/image/face.jpg");
         Mat small = bip.resizeImage(read_img, 4); //resize the image (1/4-size)
-        iio.saveImage("src/small_face.jpg", small);
-        Mat filtered = bip.doImageFiltering(small); //image filtering
-        iio.saveImage("src/filtered_face.jpg", filtered);
+//        iio.saveImage("src/mid-image/small_face.jpg", small);
 
-
-        //----------------------------------------
-        //DEMO2: Mosaicing the image
-        //Mosaicing the right-half size of the image
-        //----------------------------------------
-        Mat mosaic = bip.mosaicingImage(small,
-            new Point ((small.cols()/2), 0),
-            new Size (small.cols()/2,small.rows())
-            );
-        iio.saveImage("src/mosaiced_tsuda.jpg", mosaic);
-
-
-        //----------------------------------------
-        //DEMO3: Impose an image onto the input image
-        //----------------------------------------
-        Mat paste = iio.readImage("src/pien.PNG");
+        Mat paste = iio.readImage("src/image/pien.PNG");
         Mat paste_small = bip.resizeImage(paste, 2);
         Mat paste_result = bip.imposeImage(small, paste_small,
            new Point ( (small.cols()-paste_small.cols()-1), (small.rows()-paste_small.rows()-1) ),
            new Size (paste_small.cols(), paste_small.rows())
            );
-        iio.saveImage("src/stamped_tsuda.jpg", paste_result);
+        iio.saveImage("src/out-image/stamped.jpg", paste_result);
     }
 }
